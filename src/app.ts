e2cf5b840d775d0b2e322c23358570f0eec01e55
SagarJKaknoore <sagarkaknoor.com>
2026-08-24 18:35:56 +0530

    GET Post Patch delete Understanding

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,21 @@ import express from "express";
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from DevTinder!, Sagar Kaknoor Kaknoor");
+app.use("/user", (req, res) => {
+  res.send("Hello World");
 });
-
+app.get("/user", (req, res) => {
+  res.send("Hello User");
+});
+app.post("/user", (req, res) => {
+  res.send("User created");
+});
+app.patch("/user", (req, res) => {
+  res.send("User updated");
+});
+app.delete("/user", (req, res) => {
+  res.send("User deleted");
+});
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
